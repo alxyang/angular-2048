a1b2aa7de2801b1f509c2f55c300b66770d2c1ad
Yang, Alex <alyx.yang@gmail.com>
2014-09-23 15:30:15 -0700

    implemented core game logic and modules

---
+++ app/scripts/game/game.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('Game', ['Grid'])
+.service('GameManager', function(GridService) {
+  // Create a new game
+  this.newGame = function() {};
+  // Handle the move action
+  this.move = function() {};
+  // Update the score
+  this.updateScore = function(newScore) {
+    console.log(newScore);
+  };
+  // Are there moves left?
+  this.movesAvailable = function() {
+    return GridService.anyCellsAvailable() || 
+            GridService.tileMatchesAvailable();
+  };
+});
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,8 +25,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'test/unit/**/*.js',
     ],
 
     // list of files / patterns to exclude
